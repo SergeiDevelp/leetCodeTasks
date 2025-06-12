5a79626dd801ad6aeb9300d5f32246c8f2f04bce
ZakharovSD <zakharovsd>
2025-06-12 18:01:26 +0900

    Add solution for LeetCode 1071: use Euclid’s algorithm (GCD) and recursion

---
+++ 1071_GreatestCommonDivisorOfStrings/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _1071_GreatestCommonDivisorOfStrings;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        
+        var str1 = Console.ReadLine();
+        var str2 = Console.ReadLine();
+        
+        Console.WriteLine(GcdOfStrings(str1, str2));
+    }
+    
+    public static string GcdOfStrings(string str1, string str2)
+    {
+        if (str1 + str2 != str2 + str1)
+            return "";
+            
+        int g = Gcd(str1.Length, str2.Length);
+        return str1.Substring(0, g);
+    }
+
+    private static int Gcd(int a, int b)
+    {
+        return b == 0 ? a : Gcd(b, a % b);
+    }
+}
